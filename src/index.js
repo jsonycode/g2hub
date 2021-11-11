1313bd093de67959623eb871d5e93148a2a52814
Jay Persad <jsonycode@gmail.com>
2021-11-11 02:48:01 +0100

    ✨ 3

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ console.log("jay");
 
 
 
+function makeCoffee( sugar, milk )
+{
+    var instructions = "Boil water,";
+    
+    instructions += " pour into cup,";
+    
+    instructions += " add coffee granules,";
+    
+    instructions += " add " + sugar + " spoons of sugar,";
+    
+    instructions += " add " + milk + "% milk.";
+    
+    return instructions;
+}
 
+console.log( makeCoffee( 2, 20 ) );
 
